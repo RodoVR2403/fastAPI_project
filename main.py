882429edfb87110b20a96ec252318bdfc4589c07
Rodolfo Vargas Romo <rvargas@griddynamics.com>
2023-09-08 14:13:50 -0600

    db added

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from typing import List
+from typing import List, Optional 
 from models import User, Gender, Role 
 from uuid import UUID, uuid4
 
@@ -10,6 +10,7 @@ db: List[User] = [
     User(
         id=uuid4(), 
         first_name="Juanita", 
+        middle_name="H",
         last_name="Perez",
         gender=Gender.female,
         roles=[Role.student]
@@ -18,6 +19,7 @@ db: List[User] = [
     User(
         id=uuid4(), 
         first_name="Rodolfo", 
+        middle_name="J",
         last_name="Vargas",
         gender=Gender.male,
         roles=[Role.admin, Role.user]
@@ -27,3 +29,7 @@ db: List[User] = [
 @app.get("/")
 async def root():
     return {"Hello": "Mundo"}
+
+@app.get("/api/v1/users")
+async def fetch_users():
+    return db;
